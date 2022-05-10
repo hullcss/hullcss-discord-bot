36de3a15102d350b3997d59b95eb282367309b7b
kieranrobson <kieranrobson1999@gmail.com>
2022-05-10 01:24:31 +0100

    Added buy command

---
+++ commands/general/help.js
@@ -0,0 +1,51 @@
+const discord = require("discord.js");
+
+module.exports = {
+    name: "help",
+    aliases: [''],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+
+        if(!args[0]){
+            const embed = new discord.MessageEmbed()
+            .setTitle("HullCSS Help")
+            .setDescription("You can use `-help <category>` to get additional commands within a specfic category")
+            .setColor('GREEN')
+    
+            .addField('❓`-help`','This Command', true)
+            .addField('🎉`-help admin`','Displays Admin Commands!', true)
+            .addField('ℹ️ `-help general`', 'Displays General Commands!', true)
+            message.channel.send({ embeds: [embed] });
+        }
+    
+        if(args[0] === 'admin'){
+            const embed = new discord.MessageEmbed()
+            .setTitle("Admin Commands")
+            .setColor('GREEN')
+
+            message.channel.send({ embeds: [embed] });
+        }
+    
+
+    
+        if(args[0] === 'general'){
+            const embed = new discord.MessageEmbed()
+            .setTitle("General Comamnds")
+            .setColor('GREEN')
+
+            .addField('-link', 'Sends a link to purchase membership')
+
+
+            message.channel.send({ embeds: [embed] });
+        }
+
+    }
+}
+
+
+
