21ef7ed8dde43e00b933b4203c25a68d01400f29
kieranrobson <kieranrobson1999@gmail.com>
2022-05-10 20:19:41 +0100

    Finished welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,7 +8,7 @@ client.on('guildMemberAdd', async guildmember =>{
     const welcomechannel =client.channels.cache.get('427875246801027072')
     if(guild == guildmember.guild.id )
     {
-        welcomechannel.send(`<:hi:858281121611513897> Welcome! **${guildmember.user}** has just joined the server! Grab some roles from: `);
+        welcomechannel.send(`<:hi:858281121611513897> Welcome! **${guildmember.user}** has just joined the server! Grab some roles from: <#427873938333499404> and let us know who you are within <#427874873898041346>`);
     
     } 
 
