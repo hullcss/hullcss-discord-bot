441d615dd7cf260be0c5a6f86d10c9441866ecb8
kieranrobson <kieranrobson1999@gmail.com>
2022-05-10 22:03:11 +0100

    Removed if statement

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -6,10 +6,6 @@ const discord = require('discord.js');
 client.on('guildMemberAdd', async guildmember =>{
     const guild = client.guilds.cache.get('427865794467069962');
     const welcomechannel = client.channels.cache.get('427875246801027072')
-    if(guild == '427865794467069962')
-    {
-        welcomechannel.send(`<a:WavingHand:973689926258393169> Welcome! **${guildmember.user}** has just joined the server! Grab some roles from <#427873938333499404> and let us know who you are within <#427874873898041346>`);
-    
-    } 
+    welcomechannel.send(`<a:WavingHand:973689926258393169> Welcome! **${guildmember.user}** has just joined the server! Grab some roles from <#427873938333499404> and let us know who you are within <#427874873898041346>`);
 
 })
