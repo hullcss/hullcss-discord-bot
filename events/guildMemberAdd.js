707f4d9b9755c2ebf8796cc16f94450449438681
kieranrobson <kieranrobson1999@gmail.com>
2022-05-10 20:18:53 +0100

    Updated welcoem message and ready

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,7 +5,7 @@ const discord = require('discord.js');
 
 client.on('guildMemberAdd', async guildmember =>{
     const guild = client.guilds.cache.get('427865794467069962');
-
+    const welcomechannel =client.channels.cache.get('427875246801027072')
     if(guild == guildmember.guild.id )
     {
         welcomechannel.send(`<:hi:858281121611513897> Welcome! **${guildmember.user}** has just joined the server! Grab some roles from: `);
--- events/ready.js
+++ events/ready.js
@@ -9,9 +9,9 @@ client.on('ready', () => {
 
     const guild = client.guilds.cache.get('427865794467069962');
     setInterval(() =>{
-        const memberCount = guild.members.cache.size;
+        const memberCount = client.guilds.cache.get('427865794467069962').memberCount
         const channel = guild.channels.cache.get('906167542249308160');
         channel.setName(`Discord Members: ${memberCount.toLocaleString()}`);
-    }, 600000);
+    }, 60000);
 
    })
