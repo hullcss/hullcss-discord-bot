53546658f9fca082b1bd56a4c6520833b74845c9
kieranrobson <kieranrobson1999@gmail.com>
2022-05-10 21:57:11 +0100

    Added add emoji and changed ids

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,10 +5,10 @@ const discord = require('discord.js');
 
 client.on('guildMemberAdd', async guildmember =>{
     const guild = client.guilds.cache.get('427865794467069962');
-    const welcomechannel =client.channels.cache.get('427875246801027072')
-    if(guild == guildmember.guild.id )
+    const welcomechannel = client.channels.cache.get('427875246801027072')
+    if(guild == '427865794467069962')
     {
-        welcomechannel.send(`<:hi:858281121611513897> Welcome! **${guildmember.user}** has just joined the server! Grab some roles from: <#427873938333499404> and let us know who you are within <#427874873898041346>`);
+        welcomechannel.send(`<a:WavingHand:973689926258393169> Welcome! **${guildmember.user}** has just joined the server! Grab some roles from <#427873938333499404> and let us know who you are within <#427874873898041346>`);
     
     } 
 
