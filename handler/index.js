136cf912c5a0796830cf76a5089517c47c16da58
kieranrobson <kieranrobson1999@gmail.com>
2022-05-11 21:16:49 +0100

    Added slash command handler

--- handler/index.js
+++ handler/index.js
@@ -24,4 +24,22 @@ module.exports = async (client) => {
     const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
     eventFiles.map((value) => require(value));
 
+    const arrayOfSlashCommands = [];
+    slashCommands.map((value) => {
+        const file = require(value);
+        if (!file?.name) return;
+        client.slashCommands.set(file.name, file);
+
+        if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
+        arrayOfSlashCommands.push(file);
+    });
+    client.on("ready", async () => {
+        // Register for a single guild
+        await client.guilds.cache
+            .get("744586833135927366")
+            .commands.set(arrayOfSlashCommands);
+
+        // Register for all the guilds the bot is in
+        // await client.application.commands.set(arrayOfSlashCommands);
+    });
 };
