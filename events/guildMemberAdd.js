3745443c293cf13f9c5c4d7f93b979ba88c4f28d
Kieran Robson <32241933+KieranRobson@users.noreply.github.com>
2022-05-12 23:51:21 +0100

    Test Commit

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -6,5 +6,6 @@ const discord = require('discord.js');
 client.on('guildMemberAdd', async guildmember =>{
     const guild = client.guilds.cache.get('427865794467069962');
     const welcomechannel = client.channels.cache.get('427875246801027072')
+    
     welcomechannel.send({ content: `<a:WavingHand:973689926258393169> Welcome! **${guildmember.user}** has just joined the server! Grab some roles from <#427873938333499404> and let us know who you are within <#427874873898041346>`})
 })
