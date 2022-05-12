8e1a72681989615888943a29e2cd29b852d9583d
Kieran Robson <32241933+KieranRobson@users.noreply.github.com>
2022-05-12 23:53:02 +0100

    Added brackets to guildmember.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,7 +3,7 @@ const client = require("../index");
 const discord = require('discord.js');
 
 
-client.on('guildMemberAdd', async guildmember =>{
+client.on('guildMemberAdd', async (guildmember) =>{
     const guild = client.guilds.cache.get('427865794467069962');
     const welcomechannel = client.channels.cache.get('427875246801027072')
     
