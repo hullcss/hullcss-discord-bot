869ead6d28c39c43aa0d22336a8f9dd1aae4fdb8
Kieran <kieranrobson1999@gmail.com>
2022-06-02 17:14:03 +0100

    Fix: Update counter

--- events/ready.js
+++ events/ready.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
     client.user.setActivity(`!help | hullcss.org`, {type:"WATCHING"})
 
 
-    const guild = client.guilds.cache.get('744586833135927366');
     setInterval(() =>{
-        const memberCount = guild.members.cache.filter(m => !m.user.bot).size;
-        const channel = guild.channels.cache.get('906167542249308160');
+        const memberCount = client.guilds.cache.get('427865794467069962').memberCount
+        const channel = client.channels.cache.get('906167542249308160');
         channel.setName(`Discord Members: ${memberCount.toLocaleString()}`);
     }, 600000);
+
 })
