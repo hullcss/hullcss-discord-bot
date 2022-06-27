1813f0371d57ff9b9d047243431c0331c8f4184a
KieranRobson <kieranrobson1999@gmail.com>
2022-06-27 10:40:12 +0100

    Add Slash Commands - Buy

---
+++ SlashCommands/general/buy.js
@@ -0,0 +1,22 @@
+const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "buy",
+    description: "returns mmembership purchase link",
+    type: 'CHAT_INPUT',
+    /**
+     *
+     * @param {Client} client
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+    run: async (client, interaction, args) => {
+        const embed = new MessageEmbed()
+        .setTitle("Thanks for considering to become a paid member!")
+        .setColor('GREEN')
+        .setFooter(`Called By: ${interaction.user.tag}`)
+        .addField('You can purchase from below!','https://hulluniunion.com/shop?aid=304')
+        interaction.channel.send({ embeds: [embed] });
+    },
+};
+
