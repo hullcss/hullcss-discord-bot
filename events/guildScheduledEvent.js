4d99e0f2bca46dc57d374ca88fa3b9137a58ce60
Kieran <kieranrobson1999@gmail.com>
2022-07-04 18:56:53 +0100

    Added image to event embed and check for null end

--- events/guildScheduledEvent.js
+++ events/guildScheduledEvent.js
@@ -10,11 +10,15 @@ client.on('guildScheduledEventCreate', async guildScheduledEvent =>{
     .setFooter(`Created by ${guildScheduledEvent.creator.tag}`)
     .setTimestamp()
     .setTitle("New Event")
+    .setImage(guildScheduledEvent.image)
     .addField('Name:', `${guildScheduledEvent.name}`)
     .addField('Description', `${guildScheduledEvent.description}`)
     .addField('Location', `Please check the URL linked.`)
     .addField('Start Date and Time', ` ${guildScheduledEvent.scheduledStartAt.toLocaleDateString("en-UK")}`, true)
-    .addField('End Date and Time', `${guildScheduledEvent.scheduledEndAt.toLocaleDateString("en-UK")}`, true)
-    .addField('InviteURL', `${guildScheduledEvent.url}`)
+    if(guildScheduledEvent.scheduledEndAt !== null)
+    {
+        embed.addField('End Date and Time', `${guildScheduledEvent.scheduledEndAt.toLocaleDateString("en-UK")}`, true)
+    }
+    embed.addField('InviteURL', `${guildScheduledEvent.url}`)
     channel.send({ embeds: [embed] })
 })
